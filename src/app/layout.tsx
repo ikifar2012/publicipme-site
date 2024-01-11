8cb0a7f4dd0f1fefcba0226254c15173c4c2f690
ikifar2012 <ikifar2012@users.noreply.github.com>
2024-01-11 07:15:27 +0000

    Add Radix UI packages and Next Themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,15 +8,26 @@ export const metadata: Metadata = {
   title: 'PublicIP.me - Your Public IP Address',
   description: 'No frills, just your public IP address.',
 }
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 export default function RootLayout({
   children,
 }: {
+
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            >
+              {children}
+            </ThemeProvider>
+      </body>
     </html>
   )
 }
