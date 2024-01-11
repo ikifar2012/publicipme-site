981cee47b6edf3f4ebe08c91e3a296c0faa278ab
ikifar2012 <ikifar2012@users.noreply.github.com>
2024-01-11 02:15:19 +0000

    Inital commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'PublicIP.me - Your Public IP Address',
+  description: 'No frills, just your public IP address.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
