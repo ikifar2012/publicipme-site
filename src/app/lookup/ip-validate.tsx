6ed1bcbbd945e1125c858a0124309d91d5935973
ikifar2012 <ikifar2012@users.noreply.github.com>
2024-02-07 06:11:10 +0000

    Remove unused import and variable in ip-validate.tsx

--- src/app/lookup/ip-validate.tsx
+++ src/app/lookup/ip-validate.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Label } from '../../components/ui/label';
 import { Input } from '../../components/ui/input';
 import {z} from 'zod';
-import revalidateResults from './revalidate-results';
 const validateIpAddress = (value:any) => {
   const ipAddressSchema = z.string().ip();
   try {
