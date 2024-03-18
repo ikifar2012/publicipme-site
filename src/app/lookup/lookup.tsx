d2a1676f91404354381035da1bdb156f59ac8762
ikifar2012 <ikifar2012@users.noreply.github.com>
2024-03-18 03:36:57 +0000

    Remove whitespace

--- src/app/lookup/lookup.tsx
+++ src/app/lookup/lookup.tsx
@@ -3,11 +3,9 @@ const apiKey = process.env.API_KEY;
 export async function Lookup(LookupData:string, type:string) {
     const data = await geolocateIpAddress(LookupData, type);
     return data;
-
 }
 async function geolocateIpAddress(ipAddress: string,  type: string) {
     let lookuptype: string;
-
         switch (type) {
             case "ip":
                 lookuptype = "ipAddress";
@@ -22,7 +20,6 @@ async function geolocateIpAddress(ipAddress: string,  type: string) {
                 throw new Error("Invalid lookup type");
         }
     const url = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&${lookuptype}=${ipAddress}`;
-
     try {
         const response = await fetch(url);
         const data = await response.json();
